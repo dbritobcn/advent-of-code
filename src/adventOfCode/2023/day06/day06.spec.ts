da89db0a6b76d6ab4409281ed4add8accd24b8b2
David Brito <david.brito.sge@mango.com>
2023-12-07 00:05:54 +0100

    AoC day 06 part 2

--- src/adventOfCode/2023/day06/day06.spec.ts
+++ src/adventOfCode/2023/day06/day06.spec.ts
@@ -2,7 +2,10 @@ import { day06 } from "./day06";
 import { inputFixture } from "./test/fixture";
 
 describe("Day 06", () => {
-  it("should return 288", () => {
+  it("should return 288 without kerning", () => {
     expect(day06(inputFixture)).toBe(288);
+  });
+  it("should return 71503 with kerning", () => {
+    expect(day06(inputFixture, true)).toBe(71503);
   });
 });
