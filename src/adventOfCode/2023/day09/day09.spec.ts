f81eaee68b60a4cd1810db77a1565d1bd9034081
David Brito <david.brito.sge@mango.com>
2023-12-11 21:13:36 +0100

    Day 09 - part 02

--- src/adventOfCode/2023/day09/day09.spec.ts
+++ src/adventOfCode/2023/day09/day09.spec.ts
@@ -1,8 +1,16 @@
-import { day09Part1 } from "./day09";
+import { day09 } from "./day09";
 import { input } from "./test/fixture";
 
-describe('Day 09', () => {
-    it('should return 114', () => {
-        expect(day09Part1(input)).toBe(114);
+describe("Day 09", () => {
+  describe("Part 01", () => {
+    it("should return 114", () => {
+      expect(day09(input)).toBe(114);
     });
+  });
+
+  describe("Part 02", () => {
+    it("should return 2", () => {
+      expect(day09(input, true)).toBe(2);
+    });
+  });
 });
--- src/adventOfCode/2023/day09/day09.ts
+++ src/adventOfCode/2023/day09/day09.ts
@@ -1,6 +1,9 @@
 import { input } from "./input";
 
-const getNewValue = (sequence: number[]): number => {
+const getNewValue = (
+  sequence: number[],
+  backwards: boolean
+): number => {
   const newSequence: number[] = sequence.reduce(
     (acc: number[], _, index: number) => {
       return index === 0 ? [] : [...acc, sequence[index] - sequence[index - 1]];
@@ -8,16 +11,26 @@ const getNewValue = (sequence: number[]): number => {
     []
   );
   return newSequence.some((number: number): boolean => number !== 0)
-    ? newSequence[newSequence.length - 1] + getNewValue(newSequence)
+    ? backwards
+      ? newSequence[0] - getNewValue(newSequence, backwards)
+      : newSequence[newSequence.length - 1] + getNewValue(newSequence, backwards)
     : 0;
 };
 
-export const day09Part1 = (input: string): number => {
+export const day09 = (
+  input: string,
+  backwards: boolean = false
+): number => {
   return input
     .split("\n")
     .map((line: string): number[] => line.split(" ").map(Number))
-    .map((line: number[]) => line[line.length - 1] + getNewValue(line))
+    .map((line: number[]) =>
+      backwards
+        ? line[0] - getNewValue(line, backwards)
+        : line[line.length - 1] + getNewValue(line, backwards)
+    )
     .reduce((acc: number, currentNumber: number) => acc + currentNumber, 0);
 };
 
-// console.log("2023 - day 9 - part 01: ", day09Part1(input));
+// console.log("2023 - day 9 - part 01: ", day09(input));
+// console.log("2023 - day 9 - part 02: ", day09(input, true));
